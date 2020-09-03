4c96511e52c49062e9e01b425951f198403d4ee0
Eugene Lepeico <eugenelepeico@gmail.com>
2020-09-03 10:43:20 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jn-lp/se-lab1
+
+go 1.15
---
+++ server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+type indexRes struct {
+  Time string `json:"time"`
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	res := indexRes{ time.Now().Format(time.RFC3339) }
+
+  jsonRes, err := json.Marshal(res)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write(jsonRes)
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+
+	http.ListenAndServe(":8795", nil)
+}
