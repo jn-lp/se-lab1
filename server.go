093e38028aaff417f201b1dacc23c01ce98e2b28
v0ldemar01 <voldemarprk2001@gmail.com>
2020-09-08 17:20:10 +0300

    Adding comments

--- server.go
+++ server.go
@@ -11,16 +11,19 @@ type indexRes struct {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+// Filling response
 	res := indexRes{time.Now().Format(time.RFC3339)}
-
+// Serializing response
 	jsonRes, err := json.Marshal(res)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
+// Setting headers
 	w.Header().Set("Content-Type", "application/json")
+// Writing headers
 	w.WriteHeader(http.StatusAccepted)
+// Writing responce
 	w.Write(jsonRes)
 }
 
